4dd86a4ae555804f3e4eb410249c413f8a4dc043
Muhammad <talha18513@gmail.com>
2024-11-01 10:32:23 +0500

    I added text to the file

---
+++ Docs/Javascripts/global.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+        0: {
+            slidesPerView: 1 
+        },
+        640: {
+            slidesPerView: 2 
+        },
+        768: {
+            slidesPerView: 3 
+        },
+        1024: {
+            slidesPerView: 4 
+        }
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".button-next",
+        prevEl: ".button-prev",
+    },
+    autoplay: {
+        delay: 3000,
+    },
+    loop: true,
+});
